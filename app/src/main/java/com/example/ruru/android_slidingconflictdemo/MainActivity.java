0d0fa9cb99647fe970ef037215521a92a5df360f
梁雅茹 <2211785113@qq.com>
2019-04-28 18:30:29 +0800

    change：delete view source

--- app/src/main/java/com/example/ruru/android_slidingconflictdemo/MainActivity.java
+++ app/src/main/java/com/example/ruru/android_slidingconflictdemo/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     //SameDirection
